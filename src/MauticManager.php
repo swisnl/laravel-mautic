c8bae60cd65c43986a7474988fc36a585cb74b62
Jasper Zonneveld <jasper@swis.nl>
2022-10-11 14:17:54 +0200

    Fix docblocks/types in MauticManager

--- src/MauticManager.php
+++ src/MauticManager.php
@@ -8,55 +8,31 @@ use GrahamCampbell\Manager\AbstractManager;
 use Illuminate\Contracts\Config\Repository;
 
 /**
- * This is the mautic manager class.
- *
- * @method \Mautic\MauticApi                              connection(string|null $name = null)
- * @method \Mautic\MauticApi                              reconnect(string|null $name = null)
+ * @method \Swis\Laravel\Mautic\Client                    connection(string|null $name = null)
+ * @method \Swis\Laravel\Mautic\Client                    reconnect(string|null $name = null)
  * @method void                                           disconnect(string|null $name = null)
- * @method array<string,\Mautic\MauticApi>                getConnections()
+ * @method array<string,\Swis\Laravel\Mautic\Client>      getConnections()
  */
 class MauticManager extends AbstractManager
 {
     protected MauticFactory $factory;
 
-    /**
-     * Create a new mautic manager instance.
-     *
-     * @param  \Illuminate\Contracts\Config\Repository  $config
-     * @param  \Swis\Laravel\Mautic\MauticFactory  $factory
-     */
     public function __construct(Repository $config, MauticFactory $factory)
     {
         parent::__construct($config);
         $this->factory = $factory;
     }
 
-    /**
-     * Create the connection instance.
-     *
-     * @param  array  $config
-     * @return \Mautic\MauticApi
-     */
-    protected function createConnection(array $config)
+    protected function createConnection(array $config): Client
     {
         return $this->factory->make($config);
     }
 
-    /**
-     * Get the configuration name.
-     *
-     * @return string
-     */
     protected function getConfigName(): string
     {
         return 'mautic';
     }
 
-    /**
-     * Get the factory
-     *
-     * @return MauticFactory
-     */
     public function getFactory(): MauticFactory
     {
         return $this->factory;
