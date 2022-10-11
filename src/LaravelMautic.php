fb0d41ec0ddaab87fd498d529e9296b3f28f7c08
Thomas Wijnands <twijnands@swis.nl>
2022-10-11 13:48:40 +0200

    add return types to the method docs

--- src/LaravelMautic.php
+++ src/LaravelMautic.php
@@ -3,43 +3,74 @@
 namespace Swis\Laravel\Mautic;
 
 use Mautic\Api\Api;
+use Mautic\Api\Assets;
+use Mautic\Api\CampaignEvents;
+use Mautic\Api\Campaigns;
+use Mautic\Api\Categories;
+use Mautic\Api\Companies;
+use Mautic\Api\CompanyFields;
+use Mautic\Api\Contacts;
+use Mautic\Api\Data;
+use Mautic\Api\Devices;
+use Mautic\Api\DynamicContents;
+use Mautic\Api\Emails;
+use Mautic\Api\Files;
+use Mautic\Api\Focus;
+use Mautic\Api\Forms;
+use Mautic\Api\Messages;
+use Mautic\Api\Notes;
+use Mautic\Api\Notifications;
+use Mautic\Api\Pages;
+use Mautic\Api\Points;
+use Mautic\Api\PointTriggers;
+use Mautic\Api\Reports;
+use Mautic\Api\Roles;
+use Mautic\Api\Segments;
+use Mautic\Api\Smses;
+use Mautic\Api\Stages;
+use Mautic\Api\Stats;
+use Mautic\Api\Tags;
+use Mautic\Api\Themes;
+use Mautic\Api\Tweets;
+use Mautic\Api\Users;
+use Mautic\Api\Webhooks;
 use Mautic\Auth\AuthInterface;
 use Mautic\Auth\BasicAuth;
 use Mautic\Exception\ContextNotFoundException;
 use Mautic\MauticApi;
 
 /**
- * @method assets()
- * @method campaignEvents()
- * @method campaigns()
- * @method categories()
- * @method companies()
- * @method companyFields()
- * @method contacts()
- * @method data()
- * @method devices()
- * @method dynamicContents()
- * @method emails()
- * @method files()
- * @method focus()
- * @method forms()
- * @method messages()
- * @method notes()
- * @method notifications()
- * @method pages()
- * @method points()
- * @method pointTriggers()
- * @method reports()
- * @method roles()
- * @method segments()
- * @method smses()
- * @method stages()
- * @method stats()
- * @method tags()
- * @method themes()
- * @method tweets()
- * @method users()
- * @method webhooks()
+ * @method Assets assets()
+ * @method CampaignEvents campaignEvents()
+ * @method Campaigns campaigns()
+ * @method Categories categories()
+ * @method Companies companies()
+ * @method CompanyFields companyFields()
+ * @method Contacts contacts()
+ * @method Data data()
+ * @method Devices devices()
+ * @method DynamicContents dynamicContents()
+ * @method Emails emails()
+ * @method Files files()
+ * @method Focus focus()
+ * @method Forms forms()
+ * @method Messages messages()
+ * @method Notes notes()
+ * @method Notifications notifications()
+ * @method Pages pages()
+ * @method Points points()
+ * @method PointTriggers pointTriggers()
+ * @method Reports reports()
+ * @method Roles roles()
+ * @method Segments segments()
+ * @method Smses smses()
+ * @method Stages stages()
+ * @method Stats stats()
+ * @method Tags tags()
+ * @method Themes themes()
+ * @method Tweets tweets()
+ * @method Users users()
+ * @method Webhooks webhooks()
  */
 class LaravelMautic extends MauticApi
 {
