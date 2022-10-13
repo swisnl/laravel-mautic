7a2cbac825afa1a288828beae775c4574aed76de
rvanvelzen <rvanvelzen@swis.nl>
2022-10-13 17:17:16 +0200

    Use better mocks

--- tests/RequestTest.php
+++ tests/RequestTest.php
@@ -3,21 +3,13 @@
 use Swis\Laravel\Mautic\Facades\Mautic;
 use Swis\Laravel\Mautic\Tests\mock\User;
 
-it('can do a call or something', function () {
-    expect(true)->toBeTrue();
-
-    $response = Mautic::contacts()->get(1);
-
-    // dd($response);
-});
-
 it('creates a new instance in Mautic when the model does not have a mautic_id', function () {
     $user = new User([
         'name' => 'John Doe',
         'email' => 'john@exampel.com',
     ]);
 
-    $mock = mock(\Mautic\Api\Contacts::class)->expect(
+    $contactsMock = mock(\Mautic\Api\Contacts::class)->expect(
         itemName: fn () => 'contact'
     )->shouldReceive('create')
         ->once()
@@ -25,11 +17,7 @@ it('creates a new instance in Mautic when the model does not have a mautic_id', 
         ->andReturn(['contact' => ['id' => '1337']])
         ->getMock();
 
-    Mautic::shouldReceive('connection')
-        ->andReturnSelf();
-    Mautic::shouldReceive('contacts')
-        ->once()
-        ->andReturn($mock);
+    mockManager('contacts', $contactsMock);
 
     $user->save();
 
@@ -43,33 +31,25 @@ it('updates an existing Mautic entity when a mautic_id is found on the model', f
         'mautic_id' => '1337',
     ]);
 
-    $mock = mock(\Mautic\Api\Contacts::class)
+    $contactsMock = mock(\Mautic\Api\Contacts::class)
         ->shouldReceive('edit')
         ->once()
         ->with('1337', $user->toMauticArray())
         ->getMock();
 
-    Mautic::shouldReceive('connection')
-        ->andReturnSelf();
-    Mautic::shouldReceive('contacts')
-        ->once()
-        ->andReturn($mock);
+    mockManager('contacts', $contactsMock);
 
     $user->save();
 });
 
 it('tries to delete a user from Mautic when a user is deleted', function () {
-    $mock = mock(\Mautic\Api\Contacts::class)
+    $contactsMock = mock(\Mautic\Api\Contacts::class)
         ->shouldReceive('delete')
         ->once()
         ->with('1337')
         ->getMock();
 
-    Mautic::shouldReceive('connection')
-        ->andReturnSelf();
-    Mautic::shouldReceive('contacts')
-        ->once()
-        ->andReturn($mock);
+    mockManager('contacts', $contactsMock);
 
     $user = new User([
         'name' => 'John Doe',
@@ -80,3 +60,17 @@ it('tries to delete a user from Mautic when a user is deleted', function () {
     $user->saveQuietly();
     $user->delete();
 });
+
+function mockManager(string $methodName, \Mautic\Api\Api $api): void {
+    $clientMock = mock(\Swis\Laravel\Mautic\Client::class)
+        ->shouldReceive($methodName)
+        ->andReturn($api)
+        ->getMock();
+
+    $managerMock = mock(\Swis\Laravel\Mautic\MauticManager::class)
+        ->shouldReceive('connection')
+        ->andReturn($clientMock)
+        ->getMock();
+
+    Mautic::swap($managerMock);
+}
