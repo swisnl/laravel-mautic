4599788ff9a7445e4355bf10196a7db16bb7fd4f
Jasper Zonneveld <jasper@swis.nl>
2023-03-13 15:04:28 +0100

    test: fix NotificationTest

--- tests/NotificationTest.php
+++ tests/NotificationTest.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Notifications\Notification;
+use Swis\Laravel\Mautic\Exceptions\NotificationException;
 use Swis\Laravel\Mautic\Notifications\MauticChannel;
 use Swis\Laravel\Mautic\Notifications\MauticMessage;
 use Swis\Laravel\Mautic\Tests\mock\User;
@@ -31,7 +32,39 @@ it('can send a notification', function () {
 
     mockManager('emails', $emailsMock);
 
-    $result = $channel->send($user, $notification);
-
-    expect($result['succes'])->toBeTrue();
+    $channel->send($user, $notification);
 });
+
+it('throws when sending a notification fails', function () {
+    $user = new User([
+        'name' => 'John Doe',
+        'email' => 'john@exampel.com',
+        'mautic_id' => '12345',
+    ]);
+    $notification = new class extends Notification
+    {
+        public function toMautic($notifiable)
+        {
+            return MauticMessage::create(1)
+                ->tokens(['foo' => 'bar']);
+        }
+    };
+    $channel = new MauticChannel();
+
+    $emailsMock = mock(\Mautic\Api\Emails::class)
+        ->makePartial()
+        ->shouldReceive('sendToContact')
+        ->once()
+        ->with(1, $user->routeNotificationFor('mautic', $notification), ['tokens' => ['foo' => 'bar']])
+        ->andReturn(['errors' => [
+            [
+                'code' => 500,
+                'message' => 'Something went wrong',
+            ],
+        ]])
+        ->getMock();
+
+    mockManager('emails', $emailsMock);
+
+    $channel->send($user, $notification);
+})->throws(NotificationException::class, 'Something went wrong');
